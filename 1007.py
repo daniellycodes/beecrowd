bedef5a4f2a9989b790701299ed92f06dc4172ed
danielly paulino <98064933+daniellycodes@users.noreply.github.com>
2023-04-07 14:46:43 -0300

    Diferença

---
+++ 1007.py
@@ -0,0 +1,12 @@
+#nome: Dani(elly);
+#data: 07/04/2023;
+#problema: Diferença;
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+diferenca = (a * b - c * d)
+
+print('DIFERENCA = {}'.format(diferenca))
