07d8fc092d8f99a661bfd012d950c4ac9834a334
danielly paulino <98064933+daniellycodes@users.noreply.github.com>
2023-04-07 14:36:15 -0300

    Média 1

---
+++ 1005.py
@@ -0,0 +1,10 @@
+#nome: Dani(elly);
+#data: 07/04/2023;
+#problema: Média 1;
+
+a = float(input())
+b = float (input())
+
+media = ((3.5 * a) + (7.5 * b))/11
+
+print('MEDIA = {:.5f}'.format(media))
