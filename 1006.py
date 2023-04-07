a67dbdccecca8023a212f23e0edb93d7c0cdc13c
danielly paulino <98064933+daniellycodes@users.noreply.github.com>
2023-04-07 14:41:43 -0300

    Média 2

---
+++ 1006.py
@@ -0,0 +1,11 @@
+#nome: Dani(elly);
+#data: 07/04/2023;
+#problema: Média 2;
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+media = ((2 * a) + (3 * b) + (5 * c))/10
+
+print('MEDIA = {:.1f}'.format(media))
